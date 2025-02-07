e8e3f1d4d66ae4e7c9300bcdeeb07823f331048b
andruwka0 <andrusha_2021@outlook.com>
2025-02-07 18:50:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -98,3 +98,81 @@ async def handle_buttons(callback_query: CallbackQuery, state: FSMContext):
         await bot.answer_callback_query(callback_query.id)
     else:
         new_text = "Практика пока не реализована)))"
+
+
+
+@dp.callback_query_handler(lambda c: c.data.startswith("category_"))
+async def select_category(callback_query: CallbackQuery, state: FSMContext):
+    category = callback_query.data.split("_")[1]
+    user_id = callback_query.from_user.id
+    c.execute("UPDATE users SET category = ? WHERE user_id = ?", (category, user_id))
+    conn.commit()
+    await state.update_data(category=category)
+    await bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id,
+                                text=f"Вы выбрали категорию: {category.capitalize()}! Теперь выберите уровень.",
+                                reply_markup=level_keyboard)
+    await bot.answer_callback_query(callback_query.id)
+
+
+@dp.callback_query_handler(lambda c: c.data.startswith("level_"))
+async def select_level(callback_query: CallbackQuery, state: FSMContext):
+    level = callback_query.data.split("_")[1]
+    user_id = callback_query.from_user.id
+    c.execute("UPDATE users SET level = ? WHERE user_id = ?", (level, user_id))
+    conn.commit()
+    user_data = await state.get_data()
+    category = user_data.get("category", get_user_category(user_id))
+    print(category)
+    folder_id = 'b1gug7c74crq38i2spt2'
+    api_key = 'AQVN2VdnEpiYARjmZXK4bO4GYyeeIdPqcNba3pGY'
+    gpt_model = "yandexgpt-lite"
+
+    if category == 'business':
+        with open('terms.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        random_terms = random.sample(data["terms"], 15)
+    elif category == 'medicine':
+        with open('med.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        random_terms = random.sample(data["terms"], 15)
+    else:
+        print(12)
+        with open('baza.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        random_terms = random.sample(data['terms'], 15)
+
+    final = [term['en'] for term in random_terms]
+    system_prompt = f"Ты должен составить текст на английском языке на уровне {level} по теме {category}" \
+                    f" по данным словам. Текст длинной 8 предложений. Текст длинной не более 150 слов."
+    user_prompt = ' '.join(final)
+    body = {
+        'modelUri': f'gpt://{folder_id}/{gpt_model}',
+        'completionOptions': {'stream': False, 'temperature': 0.3, 'maxTokens': 2000},
+        'messages': [
+            {'role': 'system', 'text': system_prompt},
+            {'role': 'user', 'text': user_prompt},
+        ],
+    }
+    url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completionAsync'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Api-Key {api_key}'
+    }
+    response = requests.post(url, headers=headers, json=body)
+    operation_id = response.json().get('id')
+    url = f"https://llm.api.cloud.yandex.net:443/operations/{operation_id}"
+    while True:
+        response = requests.get(url, headers=headers)
+        if response.json()["done"]:
+            break
+        time.sleep(2)
+    data = response.json()
+    answer = data['response']['alternatives'][0]['message']['text']
+    await bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id,
+                                text=f"{answer}\n\nИспользовались слова:\n{' '.join(final)}",
+                                reply_markup=main_keyboard)
+    await state.finish()
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
